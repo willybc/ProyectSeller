35434ec5e3ad9ea644030f17ae9bc95e2d050d62
Willy <willybc06@gmail.com>
2020-05-23 05:18:12 -0300

    add page 404 and list products in front page

---
+++ 404.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head() ?>
+</head>
+
+<div id="notfound">
+		<div class="notfound">
+			<div class="notfound-404">
+				<h1>4<span>0</span>4</h1>
+			</div>
+            <div class="notfound-404">
+                <p class="m-3">La pagina que estas buscando no se encuentra disponible.</p>
+            </div>
+            <a href="<?php echo home_url(); ?>">Pagina Principal</a>
+		</div>
+</div>
+
+
+
+
